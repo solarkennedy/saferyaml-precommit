64ab5e0ce6d562aa4b9b6953b35c08f0f3b04116
Kyle Anderson <kyle@xkyle.com>
2019-03-27 21:23:45 -0700

    wip

--- saferyaml_transitioner/main.py
+++ saferyaml_transitioner/main.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 import sys
 from ruamel.yaml import YAML
+from yaml import safe_load as classic_yaml_load
+from yaml import safe_dump as classic_yaml_dump
 
 import argparse
 import io
+
+
+def yaml_sanitize(i):
+    if isinstance(i, str):
+        o = classic_yaml_dump(classic_yaml_load(str(i))).replace('\n...\n', '')
+        print(f"{i} -> {o}")
+    else:
+        o = i
+    return o
 
 
 def round_trip_lint_a_file(f):
@@ -21,6 +32,10 @@ def round_trip_lint(contents):
     stream = io.BytesIO()
     yaml=YAML()
     parsed = yaml.load(contents)
+    for p in parsed:
+        print(repr(parsed[p]))
+        parsed[p] = yaml_sanitize(parsed[p])
+        print(repr(parsed[p]))
     yaml.default_flow_style = False
     yaml.dump(parsed, stream)
     stream.seek(0)
--- tests/test_everything.py
+++ tests/test_everything.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 from saferyaml_transitioner.main import round_trip_lint
 
+def parse(i):
+    return round_trip_lint(i).decode('utf8').strip()
+
 def test_round_trip_passes_through():
     i = "foo: true"
-    assert round_trip_lint(i) == b"foo: true\n"
+    assert parse(i) == "foo: true"
 
 
 def test_round_trip_passes_through_multiline():
     i = """foo: true
-bar: baz
-"""
-    assert round_trip_lint(i) == i.encode('utf-8')
+bar: baz"""
+    assert parse(i) == i
+
+
+def test_round_trip_replaces_bad_bools():
+    i = "foo: NO"
+    assert parse(i) == "foo: false"
