583d44089632e4dfb1e83db7e4cdf290d2fbde9c
Kyle Anderson <kyle@xkyle.com>
2019-06-01 14:17:32 -0700

    Clean up tests a bit

--- tests/test_everything.py
+++ tests/test_everything.py
@@ -15,34 +15,33 @@ def test_fixes_stuff_and_returns_1(input_yaml, expected_yaml, tmpdir, capsys):
     path = tmpdir.join('input.yaml')
     path.write(input_yaml)
     ret = make_yaml_file_safer(path.strpath)
-    assert path.read() == expected_yaml
+    assert expected_yaml == path.read()
     assert ret == 1
 
 
 @pytest.mark.parametrize(
-    ('input_yaml', 'expected_yaml'),
-    (
-        ('foo: bar\n', 'foo: bar\n'),
-        ('foo: "no"\n', 'foo: "no"\n'),
-        ('foo: false\n', 'foo: false\n'),
-    ),
+    'input_yaml',
+    [
+        'foo: bar\n',
+        'foo: false\n',
+        'a__________: 1\nb: false\ncc: false\n',
+    ],
 )
-def test_is_idempotent_and_returns_0(input_yaml, expected_yaml, tmpdir, capsys):
+def test_is_idempotent_and_returns_0(input_yaml, tmpdir):
     path = tmpdir.join('input.yaml')
     path.write(input_yaml)
     ret = make_yaml_file_safer(path.strpath)
-    assert path.read() == expected_yaml
+    assert input_yaml == path.read()
     assert ret == 0
 
 
 @pytest.mark.parametrize(
-    ('input_yaml', 'expected_yaml'),
-    (
-        ('foo: bar\nfoo: baz\n', 'foo: bar\n'),
-    ),
+    'input_yaml',
+    [
+    ],
 )
 @pytest.mark.filterwarnings('ignore:')
-def test_raises_on_things_it_cant_handle(input_yaml, expected_yaml, tmpdir, capsys):
+def test_raises_on_things_that_pytest_things_will_change(input_yaml, tmpdir, capsys):
     path = tmpdir.join('input.yaml')
     path.write(input_yaml)
     with pytest.raises(YAMLSemanticChangeError):
