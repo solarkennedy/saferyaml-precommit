af774eb2b4a07dd011eb4bd3d2fa756a27f47fac
Kyle Anderson <kyle@xkyle.com>
2019-06-01 10:22:12 -0700

    Rename

--- tests/test_everything.py
+++ tests/test_everything.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from saferyaml_transitioner.main import round_trip_lint
+from saferyaml_precommit.main import round_trip_lint
 
 def test_round_trip_passes_through():
     i = "foo: true"
