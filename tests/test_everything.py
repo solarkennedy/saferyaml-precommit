12eb970c3842b1a0375caee2318b3061e4faafee
Kyle Anderson <kyle@xkyle.com>
2019-06-01 12:22:19 -0700

    Better test output

--- tests/test_everything.py
+++ tests/test_everything.py
@@ -8,26 +8,31 @@ from saferyaml_precommit.main import YAMLSemanticChangeError
     ('input_yaml', 'expected_yaml'),
     (
         ('foo:         bar\n', 'foo: bar\n'),
+        ('foo: False\n', 'foo: false\n'),
     ),
 )
-def test_fixes_stuff(input_yaml, expected_yaml, tmpdir, capsys):
+def test_fixes_stuff_and_returns_1(input_yaml, expected_yaml, tmpdir, capsys):
     path = tmpdir.join('input.yaml')
     path.write(input_yaml)
-    assert make_yaml_file_safer(path.strpath) == 1
+    ret = make_yaml_file_safer(path.strpath)
     assert path.read() == expected_yaml
+    assert ret == 1
 
 
 @pytest.mark.parametrize(
     ('input_yaml', 'expected_yaml'),
     (
         ('foo: bar\n', 'foo: bar\n'),
+        ('foo: "no"\n', 'foo: "no"\n'),
+        ('foo: false\n', 'foo: false\n'),
     ),
 )
-def test_is_idempotent(input_yaml, expected_yaml, tmpdir, capsys):
+def test_is_idempotent_and_returns_0(input_yaml, expected_yaml, tmpdir, capsys):
     path = tmpdir.join('input.yaml')
     path.write(input_yaml)
-    assert make_yaml_file_safer(path.strpath) == 0, path.read()
+    ret = make_yaml_file_safer(path.strpath)
     assert path.read() == expected_yaml
+    assert ret == 0
 
 
 @pytest.mark.parametrize(
